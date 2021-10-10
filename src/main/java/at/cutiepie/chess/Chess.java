2ddf5242d304d2dd09babf6a8d484e95eb24ab5b
Aurora <aurora@relanet.eu>
2021-10-10 14:53:34 +0200

    init

---
+++ src/main/java/at/cutiepie/chess/Chess.java
@@ -0,0 +1,17 @@
+package at.cutiepie.chess;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chess extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
